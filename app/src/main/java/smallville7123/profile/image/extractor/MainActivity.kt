374ef0d40c41f34377bab6453523be5f0489f995
Matthew Good <smallville7123@gmail.com>
2020-04-05 00:06:43 +1000

    initial commit

---
+++ app/src/main/java/smallville7123/profile/image/extractor/MainActivity.kt
@@ -0,0 +1,29 @@
+package smallville7123.profile.image.extractor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import androidx.annotation.ColorInt
+import com.madrapps.eyedropper.EyeDropper
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//        imageView.setImageResource(R.mipmap.ic_launcher)
+        textView2.text = "Selected Color: null"
+        EyeDropper(imageView, object : EyeDropper.ColorSelectionListener {
+            override fun onTouch(x: Int, y: Int) {
+                // x,y is given when you touch the targetView
+                textView3.text = "x: $x\ny: $y"
+            }
+            override fun onColorSelected(@ColorInt color: Int) {
+                // color is the color selected when you touch the targetView
+                ColorView.setBackgroundColor(color)
+                textView2.text = "Selected Color: $color"
+            }
+        })
+    }
+}
